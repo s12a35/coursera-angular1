7ccdd8a347ac0688e220bdf8d48d2a25d8cb29c2
Thomas Schüler <thomas.schueler@mind-me.de>
2016-12-10 22:46:25 +0100

    module1-solution

---
+++ module1-solution/app.js
@@ -0,0 +1,40 @@
+(function () {
+    'use strict';
+    angular.module('lunchCheck',[])
+        .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject=['$scope'];
+    function LunchCheckController($scope) {
+        $scope.lunchstr="";
+        $scope.message="";
+
+        $scope.checkLunch = function () {
+            if(arrLength() == 0){
+                $scope.message="Please enter Data first!";
+            }
+            else if(arrLength()>3){
+                $scope.message= "Too Much!!!";
+            }
+            else{
+                $scope.message= "Enjoy";
+            }
+        };
+
+        function arrLength(){
+            var al = $scope.lunchstr.split(',');
+            var counter = 0;
+            for (var i=0; i<al.length; i++){
+                if(al[i]!= ""){
+                    counter++;
+                }
+            }
+            return counter;
+        };
+
+    }
+
+})();
+/**
+ * Created by thomasschuler on 10.12.16.
+ */
+
